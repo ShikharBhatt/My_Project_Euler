7e8ab01dd21294e636c218d93002071a7983cd85
Shikhar Bhatt <shikhar.bhatt011@gmail.com>
2018-03-12 23:17:37 +0530

    Diff of sum & squares

---
+++ 6.py
@@ -0,0 +1,24 @@
+#The sum of the squares of the first ten natural numbers is,
+
+#12 + 22 + ... + 102 = 385
+#The square of the sum of the first ten natural numbers is,
+
+#(1 + 2 + ... + 10)2 = 552 = 3025
+#Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+n=100
+
+
+sum_square=((n*(n+1))/2)**2
+
+squares_sum=(n*(n+1)*(2*n+1))/6
+
+ans=sum_square-squares_sum
+
+print(ans)
+
+
+#Answer : 25164150
